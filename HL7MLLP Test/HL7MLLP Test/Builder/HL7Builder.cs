790cbc766de11665e82c11a1951b49ca0b8e552e
Louis van Zyl <louis.vanzyl@entelect.co.za>
2023-03-24 08:42:36 +0200

    Add some logic to get the builder pattern up and working

---
+++ HL7MLLP Test/HL7MLLP Test/Builder/HL7Builder.cs
@@ -0,0 +1,23 @@
+﻿namespace HL7MLLP_Test.Builder
+{
+    public static class HL7Builder
+    {
+        public static BaseHl7DataStructure WithSegmentName(this BaseHl7DataStructure hl7Data, string segmentName)
+        {
+            hl7Data.SegmentName = segmentName;
+            return hl7Data;
+        }
+
+        public static BaseHl7DataStructure WithField(this BaseHl7DataStructure hl7Data, int index, Func<Dictionary<int, BaseHL7Field>, Dictionary<int, BaseHL7Field>> segmentConfiguration)
+        {
+            segmentConfiguration(hl7Data.Fields);
+            return hl7Data;
+        }
+
+        public static BaseHL7Field WithFieldSegment(this BaseHL7Field hl7Field, int index, Func<Dictionary<int, string>, Dictionary<int, string>> componentConfiguration)
+        {
+            componentConfiguration(hl7Field.Components);
+            return hl7Field;
+        }
+    }
+}
