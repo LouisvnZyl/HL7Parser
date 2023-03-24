e9197ae67a13bae1c15820bb530896903476fa37
Louis van Zyl <louis.vanzyl@entelect.co.za>
2023-03-24 10:41:13 +0200

    Add logic for the HL7 builder

--- HL7MLLP Test/HL7MLLP Test/Builder/HL7Builder.cs
+++ HL7MLLP Test/HL7MLLP Test/Builder/HL7Builder.cs
@@ -8,16 +8,25 @@
             return hl7Data;
         }
 
-        public static BaseHl7DataStructure WithField(this BaseHl7DataStructure hl7Data, int index, Func<Dictionary<int, BaseHL7Field>, Dictionary<int, BaseHL7Field>> segmentConfiguration)
+        public static BaseHl7DataStructure WithFields(this BaseHl7DataStructure hl7Data, Func<Dictionary<int, BaseHL7Field>, Dictionary<int, BaseHL7Field>> segmentConfiguration)
         {
             segmentConfiguration(hl7Data.Fields);
             return hl7Data;
         }
 
-        public static BaseHL7Field WithFieldSegment(this BaseHL7Field hl7Field, int index, Func<Dictionary<int, string>, Dictionary<int, string>> componentConfiguration)
+        public static Dictionary<int, BaseHL7Field> WithField(this Dictionary<int, BaseHL7Field> hl7Fields, int fieldIndex, Func<Dictionary<int, string>, Dictionary<int, string>> componentConfiguration)
         {
-            componentConfiguration(hl7Field.Components);
-            return hl7Field;
+            BaseHL7Field field = new BaseHL7Field();
+            componentConfiguration(field.Components);
+
+            hl7Fields.Add(fieldIndex, field);
+            return hl7Fields;
+        }
+
+        public static Dictionary<int, string> WithComponent(this Dictionary<int, string> components, int componentIndex, string componentValue)
+        {
+            components.Add(componentIndex, componentValue);
+            return components;
         }
     }
 }
